be6ea161b74d71128d077be05bea9d33f581084a
Jacqueline Xu <jackiestuy@gmail.com>
2017-10-16 12:59:06 -0400

    one more in .h

--- musicLibrary.h
+++ musicLibrary.h
@@ -11,5 +11,6 @@ struct song_node * free_list(struct song_node * list);
 int compare_nodes(struct song_node * a, struct song_node * b);
 struct song_node * find_song(struct song_node * list);
 struct song_node * find_artist(struct song_node * list);
+struct song_node * find_random();
 void remove_song(struct song_node * list);
 struct song_node * insert_song(struct song_node * list, char * artist, char * song);
