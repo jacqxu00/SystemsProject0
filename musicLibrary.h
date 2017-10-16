6a768d6f99b15c2e498b4a487e5226c4c3438635
Jacqueline Xu <jackiestuy@gmail.com>
2017-10-16 11:01:38 -0400

    created .h

--- musicLibrary.h
+++ musicLibrary.h
@@ -1,9 +1,15 @@
-struct node {
-  int i;
-  struct node * next;
+struct song_node{ 
+	char name[256];
+	char artist[256];
+	struct song_node *next;
 };
 
 /* declare all functions */
-void print_list(struct node * list);
-struct node * insert_front(struct node * list, int x);
-struct node * free_list(struct node * list);
+void print_list(struct song_node * list);
+struct song_node * insert_front(struct song_node * list, char * artist, char * song);
+struct song_node * free_list(struct song_node * list);
+int compare_nodes(struct song_node * a, struct song_node * b);
+struct song_node * find_song(struct song_node * list);
+struct song_node * find_artist(struct song_node * list);
+void remove_song(struct song_node * list);
+struct song_node * insert_song(struct song_node * list, char * artist, char * song);
