648c33a418e30aa44d45995800fe39c6544f7b07
Jacqueline Xu <jackiestuy@gmail.com>
2017-10-18 23:58:41 -0400

    finished with most of music_library

--- music_library.c
+++ music_library.c
@@ -22,50 +22,69 @@ char first_letter(char * string) {
 }
 
 // Adds song nodes
-void add_song(struct song_node playlist[26], struct song_node * insert) {
+void add_song(struct song_node * playlist[26], struct song_node * insert) {
+	int i = letter_index(first_letter(insert->artist));
+	insert_song(playlist[i],insert);
 }
 
 // Searches for a song given song and artist name
 // Returns: a pointer to the song
-struct song_node * search_song(struct song_node playlist[26], char * song, char * person) {
-	return NULL;
+struct song_node * search_song(struct song_node * playlist[26], char * song, char * person) {
+	int i = letter_index(first_letter(person));
+	return find_song(playlist[i], song);
 }
 
 
 // Search for an artist
 // Returns: a pointer to the first song of the artist
-struct song_node * search_artist(struct song_node playlist[26], char * person) {
-	return NULL;
+struct song_node * search_artist(struct song_node * playlist[26], char * person) {
+	int i = letter_index(first_letter(person));
+	return find_artist(playlist[i], person);
 }
 
 // Print out all the entries under a certain letter
-void print_letter(struct song_node playlist[26], char letter) {
-	
+void print_letter(struct song_node * playlist[26], char letter) {
+	int i = letter_index(letter);
+	print_list(playlist[i]);
 }
 
 // Print out all the songs of a certain artist
-void print_artist(struct song_node playlist[26], char * person) {
+void print_artist(struct song_node * playlist[26], char * person) {
 	
 }
 
 // Print out the entire library
-void print_all(struct song_node playlist[26]) {
-	
+void print_all(struct song_node * playlist[26]) {
+	int i;
+	for (i = 0; i < 26; i++) {
+		print_list(playlist[i]);
+	}
 }
 
 // Shuffle - print out a series of randomly chosen songs
-void shuffle(struct song_node playlist[26]) {
-	
+void shuffle(struct song_node * playlist[26]) {
+	//QUESTION: do they not repeat
 }
 
 // Delete a song
-void delete_song(struct song_node playlist[26], struct song_node * one) {
+void delete_song(struct song_node * playlist[26], char * person, char * song) {
+	int i = letter_index(first_letter(person));
+	struct song_node * new = (struct song_node *) malloc(sizeof(struct song_node));
+	initialize_song(new, person, song);
+	remove_song(playlist[i], new);
 	
 }
 
 // Delete all the nodes
-void delete_all(struct song_node playlist[26]) {
-	
+void delete_all(struct song_node * playlist[26]) {
+	int i;
+	for (i = 0; i < 26; i++) {
+		struct song_node * current = playlist[i];
+		while (current) {
+			struct song_node * temp = remove_song(current,current);
+			current = temp;
+		}
+	}
 }
 
 int main() {
@@ -81,6 +100,7 @@ int main() {
 	printf("First letter testing\n");
 	printf("hello: %c\n",first_letter(s1));
 	printf("alphabet: %c\n",first_letter(s2));
+	
 }
 
 
