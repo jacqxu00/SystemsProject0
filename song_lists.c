5fe6e0380a45c15fb1dc1c254880db3fe32b4a21
hongwilliam <willhong20@gmail.com>
2017-10-20 11:27:27 -0400

    UPDATED FUNCTIONS, just need to fix formatting of print statements

--- song_lists.c
+++ song_lists.c
@@ -21,11 +21,22 @@ void print_list(struct song_node * list) {
 	//printf("NULL\n"); //only for testing purposes
 }
 
-void print_node(struct song_node *list){
-	//printf("\nTesting print_node: \n");
-	printf(" artist: %s \n", list->artist);
-	printf(" song: %s \n", list->name);
-	printf("\n");
+void print_node(struct song_node *list, char *person, char *song){
+	printf("\nTesting print_node: \n");
+	printf(" artist: %s \n",  person);
+	printf(" song: %s \n", song);
+	if (find_node(list, person, song) == NULL){
+		printf("Node not found! \n"); }
+}
+
+// inputs information into a song_node
+// returns: a pointer to the song_node
+struct song_node * initialize_song(char * person, char * song){
+	struct song_node *insert = (struct song_node *)malloc(sizeof(struct song_node));
+	strcpy(insert->artist, person);
+	strcpy(insert->name, song);
+	insert->next = NULL;
+	return insert;
 }
 
 //takes in pointers to two different song nodes, compares the artist strings then song title strings;
@@ -34,43 +45,45 @@ int compare_nodes(struct song_node * a, struct song_node * b) {
 	char * artist_A = a->artist;
 	char * artist_B = b->artist;
 
-	printf("\nComparing (%s", a->artist);
-	printf(": %s", a->name);
-	printf(") to (%s", b->artist);
-	printf(": %s) \n", b->name);
+	//printf("\nComparing (%s", a->artist);
+	//printf(": %s", a->name);
+	//printf(") to (%s", b->artist);
+	//printf(": %s) \n", b->name);
 	if (strcmp(artist_A, artist_B) < 0){
-		printf("Result is -1\n");
+		//printf("Result is -1\n");
 		return -1; } //this means artist a is before artist b
 
 	else{
 			if (strcmp(artist_A, artist_B) > 0){
-				printf("Result is 1\n");
+				//printf("Result is 1\n");
 				return 1; } //this means artist a comes after artist b
 
 			else{
 				char * song_A = a->name;
 				char * song_B = b->name;
 				if (strcmp(song_A, song_B) < 0){
-					printf("Result is -1\n");
+					//printf("Result is -1\n");
 					return -1; } //this means song a comes before song b
 				if (strcmp(song_A, song_B) > 0){
-					printf("Result is 1\n");
+					//printf("Result is 1\n");
 					return 1; } //this means song a comes after song b
 				}
 		}
 
-		printf("Result is 0\n");
+		//printf("Result is 0\n");
 		return 0; //if all else fails
 }
 
 // takes a pointer to the existing list and the data to be added, creates a new node and puts it at the beginning of the list;
 // returns: a pointer to the beginning of the list.
-struct song_node * insert_front(struct song_node * list, struct song_node * insert) {
+struct song_node * insert_front(struct song_node * list, char * person, char * name) {
+	struct song_node *insert = initialize_song(person, name);
 	insert->next = list;
 	return insert;
 }
 
-struct song_node * insert_end(struct song_node * list, struct song_node * insert){
+struct song_node * insert_end(struct song_node * list, char * person, char *name){
+	struct song_node *insert = initialize_song(person, name);
 	while(list && list->next != NULL){
 		list = list->next; }
 
@@ -80,14 +93,16 @@ struct song_node * insert_end(struct song_node * list, struct song_node * insert
 
 // takes a pointer to the existing list and the data to be added, creates a new node and alphabetizes it in the list;
 // returns: a pointer to the beginning of the list.
-struct song_node * insert_song(struct song_node * list, struct song_node * insert) {
+struct song_node * insert_song(struct song_node * list, char * person, char * name) {
 	struct song_node *copy = list; //so we can return beginning of the list later
+
+	struct song_node *insert = initialize_song(person, name);
 
 	//while the current song's artist and name is not "less" than the song we want to insert
 	while (list && list->next != NULL && compare_nodes(list->next, insert) != 0 && compare_nodes(list, insert) != 0){
 		//case 1: if insert should be first song in list
 		if (compare_nodes(insert,list) < 0){
-			insert_front(list, insert);
+			insert_front(list, person, name);
 		 	return copy; }
 
 		//case 2: add in middle
@@ -100,17 +115,10 @@ struct song_node * insert_song(struct song_node * list, struct song_node * inser
 
 		//case 3: if insert should be the last song in list
 		//if while loop lasted till end, we know insert end must be the case
-		insert_end(list,insert);
+		insert_end(list,person,name);
 		return copy;
 }
 
-// inputs information into a song_node
-// returns: a pointer to the song_node
-struct song_node * initialize_song(struct song_node * node, char * person, char * song){
-	strcpy(node->artist, person);
-	strcpy(node->name, song);
-	return node;
-}
 
 // takes a pointer to the existing list, goes through the entire list freeing each node;
 // returns: a pointer to the beginning of the list (which should be NULL by then).
@@ -196,14 +204,16 @@ struct song_node * find_random(struct song_node * list) {
 		i++; }
 
 	//print_list(list); //for testing purposes
-	print_node(copy); //for testing purposes
+	print_node(copy, copy->artist, copy->name); //for testing purposes
 	return copy;
 }
 
 // takes a pointer to the existing list and a node to be removed, goes through entire list until node matches and removes it;
 // returns: pointer to the beginning of the edited list
-struct song_node * remove_song(struct song_node * list, struct song_node * one) {
+struct song_node * remove_song(struct song_node * list, char * person, char *name) {
 	struct song_node *copy = list; //so we can return beginning of the list later
+
+	struct song_node *one = initialize_song(person, name);
 
 	printf("\nRemoving (%s", one->artist);
 	printf(": %s) \n", one->name);
@@ -235,54 +245,44 @@ struct song_node * remove_song(struct song_node * list, struct song_node * one) 
 int main(){
 
 	//more thorough testing
-	struct song_node *nas_song1 = (struct song_node *)malloc(sizeof(struct song_node));
-	struct song_node *nas_song2 = (struct song_node *)malloc(sizeof(struct song_node));
-	struct song_node *jayz_song1 = (struct song_node *)malloc(sizeof(struct song_node));
-	struct song_node *jayz_song2 = (struct song_node *)malloc(sizeof(struct song_node));
-	struct song_node *radiohead_song1 = (struct song_node *)malloc(sizeof(struct song_node));
-	struct song_node *nirvana_song1 = (struct song_node *)malloc(sizeof(struct song_node));
+	struct song_node *list = initialize_song("Jay Z", "Dirt Off Your Shoulder");
 
-	initialize_song(nas_song1, "Nas", "NY State of Mind");  //song 3
-	initialize_song(nas_song2, "Nas", "The World is Yours"); //song 4
-	initialize_song(jayz_song1, "Jay Z", "Ninety Nine Problems"); //song 2
-	initialize_song(jayz_song2, "Jay Z", "Dirt Off Your Shoulder"); //song 1
-	initialize_song(radiohead_song1, "Radiohead", "No Surprises"); //song 6
-	initialize_song(nirvana_song1, "Nirvana", "Lithium"); //song 5
-
-	//list parameter should be the first song to work
-	insert_song(jayz_song2, jayz_song1);
-	insert_song(jayz_song2, nas_song1);
-	insert_song(jayz_song2, nas_song2);
-	insert_song(jayz_song2, nirvana_song1);
-	insert_song(jayz_song2, radiohead_song1);
+	insert_song(list, "Jay Z", "Ninety Nine Problems");
+	insert_song(list, "Nirvana", "Lithium");
+	insert_song(list, "Nas", "NY State of Mind");
+	insert_song(list, "Radiohead", "No Surprises");
+	insert_song(list, "Nas", "The World Is Yours");
 
 	//testing print node
-	print_node(jayz_song2);
-	print_node(nas_song2);
+	print_node(list, "Nirvana", "Lithium");
+	print_node(list, "Jay Z", "Ninety Nine Problems");
+	print_node(list, "Tupac", "Hit Em Up");
 
 	//testing find node
-	find_node(jayz_song2, "Nirvana", "Lithium");
-	find_node(jayz_song2, "Tupac", "Changes");
+	find_node(list, "Nirvana", "Lithium");
+	find_node(list, "Tupac", "Changes");
 
 	//testing find artist
-	find_artist(jayz_song2, "Radiohead");
-	find_artist(jayz_song2, "Nas");
-	find_artist(jayz_song2, "Biggie");
+	find_artist(list, "Radiohead");
+	find_artist(list, "Nas");
+	find_artist(list, "Biggie");
 
 	//testing compare nodes
-	compare_nodes(nas_song1, nas_song2);
-	compare_nodes(nirvana_song1, radiohead_song1);
-	compare_nodes(radiohead_song1, nas_song1);
+	//compare_nodes(nas_song1, nas_song2);
+	//compare_nodes(nirvana_song1, radiohead_song1);
+	//compare_nodes(radiohead_song1, nas_song1);
+
 
 	srand(time(NULL));
-	find_random(jayz_song2);
-	find_random(jayz_song2);
-	find_random(jayz_song2);
+	find_random(list);
+	find_random(list);
+	find_random(list);
+
 
 	//testing random
-	remove_song(jayz_song2, nirvana_song1);
-	print_list(jayz_song2);
+	remove_song(list, "Radiohead", "No Surprises"); //have a null case
+	print_list(list);
 
 	//testing free list
-	free_list(jayz_song2);
+	free_list(list);
 }
--- song_lists.h
+++ song_lists.h
@@ -6,15 +6,15 @@ struct song_node{
 
 /* declare all functions */
 void print_list(struct song_node * list);
-void print_node(struct song_node *list);
+void print_node(struct song_node *list, char *person, char *song);
 int compare_nodes(struct song_node * a, struct song_node * b);
-struct song_node * insert_front(struct song_node * list, struct song_node * insert);
-struct song_node * insert_end(struct song_node * list, struct song_node * insert);
-struct song_node * insert_song(struct song_node * list, struct song_node * insert);
-struct song_node * initialize_song(struct song_node * node, char * person, char * song);
+struct song_node * insert_front(struct song_node * list, char *person, char *song);
+struct song_node * insert_end(struct song_node * list, char *person, char *song);
+struct song_node * insert_song(struct song_node * list, char *person, char *song);
+struct song_node * initialize_song(char * person, char * song);
 struct song_node * free_list(struct song_node * list);
 struct song_node * find_node(struct song_node * list, char * person, char * song);
 struct song_node * find_artist(struct song_node * list, char * person);
 int length_of(struct song_node * list);
 struct song_node * find_random(struct song_node * list);
-struct song_node * remove_song(struct song_node * list, struct song_node * one);
+struct song_node * remove_song(struct song_node * list, char *person, char *song);
